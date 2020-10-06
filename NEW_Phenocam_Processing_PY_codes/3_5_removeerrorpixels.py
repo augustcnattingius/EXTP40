ff4da4b06d65946e72ada2f5744cf9391b9b6088
Your Name <stoffekarlsson96@hotmail.com>
2020-10-06 14:00:03 +0200

    Inget funkar

--- NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
+++ NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
@@ -4,7 +4,6 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 im = Image.open("SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg")
 #print (im.size)
-#im.show()
 
 checkCount = 0
 extraCheck = 50
@@ -15,22 +14,26 @@ while checkCount < extraCheck or error == True:
   tempheight = im.height - 1
   temppix = (tempwidth, tempheight)
   if im.getpixel(temppix) != firstPix: 
+    print('no error')
     error == False
   checkCount = +1
 
-w = im.width
-h = im.height
+w = im.width - 1
+h = im.height - 1
 
-while error == True:
+rowCount = 0
+colCount = 0
+while error == True: #if we know there are errors => start changing
+  print('error true')
+  if im.getpixel(w, h - rowCount) == firstPix: #if first pixel to the right is wrong we know the row needs to be fixed
+    while colCount < (im.width - 1) or im.getpixel(w - colCount, h-rowCount) != firstPix: #make sure not out of bounds or remove correct photos
+      im.putpixel(im.getpixel(w - colCount, h-rowCount),(255,255,255))
+      colCount = + 1
+      #thatPix = (255, 255, 255) #ändra till numpy.nan senare
+  else:
+    error = False
     
-    #if(getpixValue == firstPix):
-        #thatPix = (255, 255, 255) #ändra till numpy.nan senare
-    #else:
-        #error = False
+  colCount = im.width - 1
+  rowCount = + 1
 
-    #if(w == 0):
-       # w = width
-        #h = height - 1
-    #else:
-        #w = w - 1
-
+im.show()
