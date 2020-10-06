3f4a6dbd24972828a10827cc27739bae704d91fa
augustcnattingius <august.cnattingius@live.se>
2020-10-06 09:37:11 +0200

    3_5

---
+++ NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
@@ -0,0 +1,37 @@
+import glob
+import os
+import shutil
+#import cv2
+import numpy as np
+from datetime import datetime
+from PIL import Image
+
+im = Image.open("/h/d2/m/au6787cn-s/EXTP40/EXTP40/pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1200.jpg")
+print (im)
+
+#checkCount = 0
+#extraCheck = 50
+#error = True
+#width, height, r  # = Image.size()
+#irstPix #= get pixel value
+#while checkNumb < extraCheck or error == True:
+    # if getPixelValue(height, width-checkCount) == firstPix
+  #  else
+ #   error == False
+
+#w = width
+#h = height
+
+#while error == True:
+    
+ #   if(getpixValue == firstPix):
+        #thatPix = (255, 255, 255) #ändra till numpy.nan senare
+  #  else:
+   #     error = False
+
+    #if(w == 0):
+     #   w = width
+      #  h = height - 1
+    #else:
+     #   w = w - 1
+
