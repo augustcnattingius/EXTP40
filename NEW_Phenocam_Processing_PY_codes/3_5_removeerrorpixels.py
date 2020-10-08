56e1a607ffdea44547714413a1e13d510d92333b
Emelie Ulin <emelieulin@Emelies-MacBook-Air.local>
2020-10-08 13:09:40 +0200

    3.5 updates

--- NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
+++ NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
@@ -4,40 +4,41 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import os.path
 import numpy as np
+from cv2 import cv2
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-im = Image.open(os.path.join(script_dir,"pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"))
-im.show()
-newIm = im.load()
-width, height = im.size
+#img = Image.open(os.path.join(script_dir,"pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"))
+im = cv2.imread(os.path.join(script_dir,"pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"), 1)
 
 checkCount = 0
 extraCheck = 25
 error = True
-
+height = im.shape[0]
+width = im.shape[1]
 w = width - 1
 h = height - 1
-
 rowCount = 0
 colCount = 0
+
 while error is True and rowCount <= h: #if we know there are errors => start changing
-  firstPix = im.getpixel((w, h-rowCount))
+  firstPix = im[h-rowCount, w]
   #check first if there actually are errors on that row
   while checkCount < extraCheck:
-    print("check 1")
     tempwidth = width - 1 - checkCount
     tempheight = height - 1
-    temppix = (tempwidth, tempheight)
-    if im.getpixel(temppix) != firstPix: 
-      print('no error')
+    if (im[tempheight, tempwidth]!=firstPix).all(): 
       error == False
     checkCount = checkCount + 1
 
-  while error is True and (colCount <= w or im.getpixel((w - colCount, h - rowCount)) == firstPix): #something is odd here
-    newIm[(w - colCount), (h-rowCount)] = (255, 0, 0) #doesn't really work with transparancy but it changes the pixel to plack
-    colCount = colCount + 1
+  #while error is True and (colCount <= w and (im[h - rowCount, w - colCount] == firstPix).all()): #something is odd here
+  #  im[h-rowCount, w - colCount] = np.nan
+  #  colCount = colCount + 1
+  im[:, (h-rowCount)] = np.nan
+
+  if(im[w, (h-rowCount)] == np.nan):
+    print("success")
   
-  colCount = width - 1
+
+  colCount = 0
   rowCount = rowCount + 1
 
-im.show()
