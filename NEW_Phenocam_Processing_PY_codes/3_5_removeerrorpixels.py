6e173a681b8dfd73013fec3cd4a795430821df72
Emelie Ulin <emelieulin@Emelies-MacBook-Air.local>
2020-10-14 15:25:57 +0200

    task1 done? 3.5 changed

--- NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
+++ NEW_Phenocam_Processing_PY_codes/3_5_removeerrorpixels.py
@@ -4,40 +4,42 @@ from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import os.path
 import numpy as np
+from cv2 import cv2
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-im = Image.open(os.path.join(script_dir,"/Users/magnus/Desktop/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"))
-width, height = im.size
+#img = Image.open(os.path.join(script_dir,"pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"))
+img = cv2.imread(os.path.join(script_dir,"pictures/Asa/SWE-ASA-NYB-FOR-P01_20170107_007_1400.jpg"), 1)
+#convert to cv2 np.array ^ because that's what's used later, np.array should be able to have nan values
+#OBS note!!!  cv2 save the image in BGR and not RGB, so you have to convert, but a conversion is
+#done in stage 4 so I haven't done it here yet
+
+im = img.astype(float)
 
 checkCount = 0
 extraCheck = 25
 error = True
-firstPix = im.getpixel((width-1, height-1))
-while checkCount < extraCheck:
-  print (im.size)
-  tempwidth = width - 1 - checkCount
-  tempheight = height - 1
-  temppix = (tempwidth, tempheight)
-  if im.getpixel(temppix) != firstPix: 
-    print('no error')
-    error == False
-  checkCount += 1
-
+bigWhile = True
+height = im.shape[0]
+width = im.shape[1]
 w = width - 1
 h = height - 1
-
 rowCount = 0
 colCount = 0
-while error is True: #if we know there are errors => start changing
-  print('second check')
-  if im.getpixel(w, h - rowCount) == firstPix: #if first pixel to the right is wrong we know the row needs to be fixed
-    while colCount < (width - 1) or im.getpixel(w - colCount, h - rowCount) != firstPix: #make sure not out of bounds or remove correct photos
-      im[w - colCount, h-rowCount] = float(np.nan) #HERE IS THE PROBLEM
-      colCount = colCount + 1
-  else:
-    error = False
-    
-  colCount = width - 1
-  rowCount += 1
 
-im.show()
+while bigWhile is True and rowCount <= h: #if we know there are errors => start changing
+  firstPix = im[h-rowCount, w]
+  #check first if there actually are errors on that row
+  while checkCount < extraCheck:
+    tempwidth = width - 1 - checkCount
+    tempheight = height - 1
+    if (im[tempheight, tempwidth]!=firstPix).all(): 
+      bigWhile == False
+      error = False
+    checkCount = checkCount + 1
+
+  #If there are errors => replace that whole row with np.nan
+  if error is True:
+    im[(h-rowCount), :] = np.nan #tried with only np.nan with no difference
+  
+  colCount = 0
+  rowCount = rowCount + 1
